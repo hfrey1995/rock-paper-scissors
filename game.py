18d9607dc3eece09f710100d0e82bc86b62b4e59
hfrey1995 <108554648+hfrey1995@users.noreply.github.com>
2022-07-13 11:53:37 -0400

    Setup RPS game

--- game.py
+++ game.py
@@ -5,3 +5,6 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+# this is the "game.py" file...
+
+print("Rock, Paper, Scissors, Shoot!")
