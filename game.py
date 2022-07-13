fd2d949ed57dfa1378cd73336829f0ae4af4dd3f
hfrey1995 <108554648+hfrey1995@users.noreply.github.com>
2022-07-13 12:36:01 -0400

    Make a Plan

--- game.py
+++ game.py
@@ -8,3 +8,34 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+# USER INPUTS
+
+
+
+# VALIDATE USER INPUTS
+
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+
+
+# DISPLAY RESULTS
+
+
+# -------------------
+# Welcome 'Player One' to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
