9fb04eedb77f56055eed329c316d7b55d5aebfae
hfrey1995 <108554648+hfrey1995@users.noreply.github.com>
2022-07-13 13:24:53 -0400

    User Choice

--- game.py
+++ game.py
@@ -12,7 +12,11 @@ print("Rock, Paper, Scissors, Shoot!")
 
 # USER INPUTS
 
+user_choice = input("Please make a selection ('rock', 'paper', 'scissors):")
 
+# You chose: 'rock'
+print ("You chose:", user_choice)
+print (f"You chose: '{user_choice}' ")
 
 # VALIDATE USER INPUTS
 
